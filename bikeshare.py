1aefc1b038d0a87813c704c53da16af2fa0225ef
cammy <Kimmie30121995@gmail.com>
2024-06-07 19:56:08 +0700

    refactoring: edit name of function display in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -152,7 +152,7 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-def display_data(df):
+def raw_data(df):
     view_data = input("Would you like to view 5 rows of individual trip data? Enter yes or no?").lower()
     start_loc = 0
     while (view_data=='yes'):
@@ -170,7 +170,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        display_data(df)
+        raw_data(df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() == 'no':
             break
